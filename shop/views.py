a8ce298f0d82f4b0740165a34ea93ad3032ad277
Alexander <hostischev54@gmail.com>
2025-05-29 15:35:31 +0300

    added index page

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Product
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'shop/index.html', {'products': products})
